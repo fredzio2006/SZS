4625a268fa575f4a4fe3eaeea88fa0ade416ac5b
Krystian P <a@example.com>
2023-12-28 17:18:26 +0100

    added phantom spawn in The End

---
+++ src/main/java/pl/krystian/szsmod/world/gen/ModEntityGeneration.java
@@ -0,0 +1,55 @@
+package pl.krystian.szsmod.world.gen;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraftforge.common.world.MobSpawnInfoBuilder;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ModEntityGeneration {
+    public static void onEntitySpawn(final BiomeLoadingEvent event){
+        addEntityToSpecificBiomes(event, EntityType.PHANTOM,10, 1, 5,
+                Biomes.THE_END, Biomes.SMALL_END_ISLANDS, Biomes.END_BARRENS, Biomes.END_HIGHLANDS, Biomes.END_MIDLANDS);
+        addEntityToSpecificBiomes(event, EntityType.ENDERMAN,60, 10, 20,
+                Biomes.THE_END, Biomes.SMALL_END_ISLANDS, Biomes.END_BARRENS, Biomes.END_HIGHLANDS, Biomes.END_MIDLANDS);
+    }
+
+
+
+    private static void addEntityToAllBiomesExceptThese(BiomeLoadingEvent event, EntityType<?> type,
+                                                        int weight, int minCount, int maxCount, RegistryKey<Biome>... biomes) {
+        // Goes through each entry in the biomes and sees if it matches the current biome we are loading
+        boolean isBiomeSelected = Arrays.stream(biomes).map(RegistryKey::getLocation)
+                .map(Object::toString).anyMatch(s -> s.equals(event.getName().toString()));
+
+        if(!isBiomeSelected) {
+            addEntityToAllBiomes(event.getSpawns(), type, weight, minCount, maxCount);
+        }
+    }
+
+    private static void addEntityToSpecificBiomes(BiomeLoadingEvent event, EntityType<?> type,
+                                                  int weight, int minCount, int maxCount, RegistryKey<Biome>... biomes) {
+        // Goes through each entry in the biomes and sees if it matches the current biome we are loading
+        boolean isBiomeSelected = Arrays.stream(biomes).map(RegistryKey::getLocation)
+                .map(Object::toString).anyMatch(s -> s.equals(event.getName().toString()));
+
+        if(isBiomeSelected) {
+            addEntityToAllBiomes(event.getSpawns(), type, weight, minCount, maxCount);
+        }
+    }
+
+    private static void addEntityToAllBiomes(MobSpawnInfoBuilder spawns, EntityType<?> type,
+                                             int weight, int minCount, int maxCount) {
+        List<MobSpawnInfo.Spawners> base = spawns.getSpawner(type.getClassification());
+        base.add(new MobSpawnInfo.Spawners(type,weight, minCount, maxCount));
+    }
+}
+
+
+
+
